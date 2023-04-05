a3a08a34f838161dc6c842ce761b393cf693737a
Priyapius <126758696+Priyapius@users.noreply.github.com>
2023-04-05 13:33:14 +0530

    Day01

---
+++ src/Day01/Program1.java
@@ -0,0 +1,14 @@
+package Day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		driver = new ChromeDriver();
+
+	}
+
+}
---
+++ src/Day01/Program2.java
@@ -0,0 +1,14 @@
+package Day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		driver = new EdgeDriver();
+
+	}
+
+}
